bcea867339d60cb589e142e37243de4786cbd4a3
Jack <jackmurray445@gmail.com>
2019-07-17 18:28:25 -0700

    Post component now loads posts from API

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Posts() {
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+
+
+
+
+    const data = await fetch(
+      "http://lara-post.test/api/posts"
+    );
+
+    const posts = await data.json();
+    setPosts(posts.data);
+  };
+
+
   return (
     <div>
         <h1>Posts</h1>
+        {posts.map(data => (
+          <div key ={data.id} className="card post-card">
+            <div className="card-body">
+              <h5 className="card-title">{data.name}</h5>
+              <p className="card-text">{data.content}</p>
+            </div>
+          </div>
+        ))}
     </div>
   );
 }
